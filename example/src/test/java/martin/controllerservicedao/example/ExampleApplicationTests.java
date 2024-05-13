5ea8c873c1c2abe6931d00823467a8fb0e8cf34f
Luarkenbb <43433457+Luarkenbb@users.noreply.github.com>
2024-05-13 19:55:10 +0800

    Init Project
    Maven Project (Java)
    SpringBoot 3.2.5
    Java: 21
    Dependencies:
    Lombok
    MySQL Driver
    Spring Data JPA
    Spring Web

---
+++ example/src/test/java/martin/controllerservicedao/example/ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package martin.controllerservicedao.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
