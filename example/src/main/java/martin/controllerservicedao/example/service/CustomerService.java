6c62a1fb11a492728636494d78708e84788d10df
Luarkenbb <43433457+Luarkenbb@users.noreply.github.com>
2024-05-15 17:50:22 +0800

    change settings

--- example/src/main/java/martin/controllerservicedao/example/service/CustomerService.java
+++ example/src/main/java/martin/controllerservicedao/example/service/CustomerService.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import martin.controllerservicedao.example.dao.CustomerEnquiryRepository;
@@ -18,6 +19,7 @@ public class CustomerService {
 	
 	private final CustomerEnquiryRepository customerEnquiryRepository;
 	
+	@Autowired
 	public CustomerService(CustomerEnquiryRepository customerEnquiryRepository) {
 		this.customerEnquiryRepository = customerEnquiryRepository;
 	}
