08f53ead737eef2d3d2b4ae272d24f1b4d424a07
Luarkenbb <43433457+Luarkenbb@users.noreply.github.com>
2024-05-15 13:52:33 +0800

    Update ResponseUtils.java

--- example/src/main/java/martin/controllerservicedao/example/utils/ResponseUtils.java
+++ example/src/main/java/martin/controllerservicedao/example/utils/ResponseUtils.java
@@ -1,6 +1,7 @@
 package martin.controllerservicedao.example.utils;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.http.HttpStatus;
@@ -11,6 +12,18 @@ public class ResponseUtils {
 		Map<String, Object> responseBody = new HashMap<>();
 		responseBody.put("status", Map.of("code", 0, "msg", "ok"));
 		responseBody.put("payload", Map.of("data",obj == null ? "" : obj));
+		
+		return ResponseEntity.status(HttpStatus.OK).body(responseBody);
+	}
+	
+	public static <T> ResponseEntity<Map<String, Object>> successListResult(List list){
+		Map<String, Object> responseBody = new HashMap<>();
+		responseBody.put("status", Map.of("code", 0, "msg", "ok"));
+		if(list.size() != 0) {
+			responseBody.put("payload", Map.of("data", list));
+		}else {
+			responseBody.put("payload", Map.of("data", null));
+		}
 		
 		return ResponseEntity.status(HttpStatus.OK).body(responseBody);
 	}
