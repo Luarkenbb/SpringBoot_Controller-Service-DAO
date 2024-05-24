da54c29a7abdb268ded0c5fc368aa9fff493d467
Luarkenbb <43433457+Luarkenbb@users.noreply.github.com>
2024-05-25 07:18:47 +0800

    Update ResponseUtils.java

--- example/src/main/java/martin/controllerservicedao/example/utils/ResponseUtils.java
+++ example/src/main/java/martin/controllerservicedao/example/utils/ResponseUtils.java
@@ -6,23 +6,30 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
+	
 public class ResponseUtils {
+	private static final String PAYLOAD = "payload";
+	private static final String STATUS = "status";
+	private static final String CODE = "code";
+	private static final String DATA = "data";
+	private static final String MESSAGE = "msg";
+	private static final String MESSAGE_OK = "ok";
+	
 	public static <T> ResponseEntity<Map<String, Object>> successSingleResult(Object obj){
 		Map<String, Object> responseBody = new HashMap<>();
-		responseBody.put("status", Map.of("code", 0, "msg", "ok"));
-		responseBody.put("payload", Map.of("data",obj == null ? "" : obj));
+		responseBody.put(STATUS, Map.of(CODE, 0, MESSAGE, MESSAGE_OK));
+		responseBody.put(PAYLOAD, Map.of(DATA, obj == null ? "" : obj));
 		
 		return ResponseEntity.status(HttpStatus.OK).body(responseBody);
 	}
 	
 	public static <T> ResponseEntity<Map<String, Object>> successListResult(List list){
 		Map<String, Object> responseBody = new HashMap<>();
-		responseBody.put("status", Map.of("code", 0, "msg", "ok"));
-		if(list.size() != 0 && list != null) {
-			responseBody.put("payload", Map.of("data", list));
+		responseBody.put(STATUS, Map.of(CODE, 0, MESSAGE, MESSAGE_OK));
+		if(list != null && list.size() != 0) {
+			responseBody.put(PAYLOAD, Map.of(DATA, list));
 		}else {
-			responseBody.put("payload", Map.of("data", "nodata"));
+			responseBody.put(PAYLOAD, Map.of(DATA, "nodata"));
 		}
 		
 		return ResponseEntity.status(HttpStatus.OK).body(responseBody);
