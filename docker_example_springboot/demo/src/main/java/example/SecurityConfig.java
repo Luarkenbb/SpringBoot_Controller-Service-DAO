2525dc08926a4a0e2f3430830db4f26a5ec3f1d0
Luarkenbb <43433457+Luarkenbb@users.noreply.github.com>
2024-07-30 22:48:02 +0800

    fuck fuck fuck i dont want to check the rediect path.
    damn

--- docker_example_springboot/demo/src/main/java/example/SecurityConfig.java
+++ docker_example_springboot/demo/src/main/java/example/SecurityConfig.java
@@ -1,29 +1,79 @@
 package example;
 
-
-
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig{
+@KeycloakConfiguration
+public class SecurityConfig {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) {
+        KeycloakAuthenticationProvider keycloakAuthenticationProvider = new KeycloakAuthenticationProvider();
+        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
+        auth.authenticationProvider(keycloakAuthenticationProvider);
+    }
+
+    @Bean
+    public KeycloakSpringBootConfigResolver keycloakConfigResolver() {
+        return new KeycloakSpringBootConfigResolver();
+    }
+
+    @Bean
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new NullAuthenticatedSessionStrategy();
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.cors(Customizer.withDefaults()); // Enable CORS
-        http.csrf(csrf -> csrf.disable());// Disable CSRF
+        http.csrf(csrf -> csrf.disable()); // Disable CSRF
 
         http
             .authorizeHttpRequests(authorizeRequests -> authorizeRequests // Require all requests to be authorized
-                .anyRequest().permitAll()
-            ); 
+                .requestMatchers("/admin/**").hasRole("admin")
+                .requestMatchers("/user/**").hasRole("user")
+                .anyRequest().authenticated()
+            )
+            .oauth2Login(Customizer.withDefaults()); // Enable OAuth2 login
 
         return http.build();
     }
-    
+
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        return new InMemoryClientRegistrationRepository(keycloakClientRegistration());
+    }
+
+    private ClientRegistration keycloakClientRegistration() {
+        return ClientRegistration.withRegistrationId("admin")
+                .clientId("admin")
+                .clientSecret("keycloak")
+                .scope("openid", "profile", "email")
+                .authorizationUri("http://localhost:4000/auth/realms/backend/protocol/openid-connect/auth")
+                .tokenUri("http://localhost:4000/auth/realms/backend/protocol/openid-connect/token")
+                .userInfoUri("http://localhost:4000/auth/realms/backend/protocol/openid-connect/userinfo")
+                .userNameAttributeName("preferred_username")
+                .clientName("admin")
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .redirectUri("{baseUrl}/login/oauth2/code/{registrationId}")
+                .build();
+    }
 }
