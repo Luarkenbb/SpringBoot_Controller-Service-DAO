4ce3200dcb2b8f5f09625df35f9b3ae625482de2
Luarkenbb <43433457+Luarkenbb@users.noreply.github.com>
2024-08-03 22:09:39 +0800

    damn damn damn
    oh yeah

--- docker_example_springboot/demo/src/main/java/example/SecurityConfig.java
+++ docker_example_springboot/demo/src/main/java/example/SecurityConfig.java
@@ -4,8 +4,10 @@ import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
 import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -24,22 +26,33 @@ import org.springframework.security.web.authentication.session.SessionAuthentica
 @KeycloakConfiguration
 public class SecurityConfig {
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) {
-        KeycloakAuthenticationProvider keycloakAuthenticationProvider = new KeycloakAuthenticationProvider();
-        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
-        auth.authenticationProvider(keycloakAuthenticationProvider);
-    }
+    @Value("${spring.security.oauth2.client.registration.keycloak.client-id}")
+    private String clientId;
 
-    @Bean
-    public KeycloakSpringBootConfigResolver keycloakConfigResolver() {
-        return new KeycloakSpringBootConfigResolver();
-    }
+    @Value("${spring.security.oauth2.client.registration.keycloak.client-secret}")
+    private String clientSecret;
 
-    @Bean
-    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
-        return new NullAuthenticatedSessionStrategy();
-    }
+    @Value("${spring.security.oauth2.client.registration.keycloak.scope}")
+    private String[] scopes;
+
+    @Value("${spring.security.oauth2.client.provider.keycloak.authorization-uri}")
+    private String authorizationUri;
+
+    @Value("${spring.security.oauth2.client.provider.keycloak.token-uri}")
+    private String tokenUri;
+
+    @Value("${spring.security.oauth2.client.provider.keycloak.user-info-uri}")
+    private String userInfoUri;
+
+    @Value("${spring.security.oauth2.client.provider.keycloak.user-name-attribute}")
+    private String userNameAttribute;
+
+    @Value("${spring.security.oauth2.client.registration.keycloak.redirect-uri}")
+    private String redirectUri;
+
+    @Value("${spring.security.oauth2.client.registration.keycloak.client-name}")
+    private String clientName;
+    
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -53,6 +66,7 @@ public class SecurityConfig {
                 .anyRequest().authenticated()
             )
             .oauth2Login(Customizer.withDefaults()); // Enable OAuth2 login
+        http.oauth2ResourceServer().jwt();
 
         return http.build();
     }
@@ -63,17 +77,17 @@ public class SecurityConfig {
     }
 
     private ClientRegistration keycloakClientRegistration() {
-        return ClientRegistration.withRegistrationId("springboot_client")
-                .clientId("springboot_client")
-                .clientSecret("admin")
-                .scope("openid", "profile", "email")
-                .authorizationUri("http://localhost:4000/realms/backend/protocol/openid-connect/auth")
-                .tokenUri("http://localhost:4000/realms/backend/protocol/openid-connect/token")
-                .userInfoUri("http://localhost:4000/realms/backend/protocol/openid-connect/userinfo")
-                .userNameAttributeName("preferred_username")
-                .clientName("springboot_client")
+        return ClientRegistration.withRegistrationId("keycloak")
+                .clientId(clientId)
+                .clientSecret(clientSecret)
+                .scope(scopes)
+                .authorizationUri(authorizationUri)
+                .tokenUri(tokenUri)
+                .userInfoUri(userInfoUri)
+                .userNameAttributeName(userNameAttribute)
+                .clientName(clientName)
                 .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-                .redirectUri("{baseUrl}/login/oauth2/code/{registrationId}")
+                .redirectUri(redirectUri)
                 .build();
     }
 }
