5dc7b8887842d8fa1ce98150cf46c10a459acb88
Luarkenbb <43433457+Luarkenbb@users.noreply.github.com>
2024-08-08 00:02:26 +0800

    damn
    damn damn

--- docker_example_springboot/demo/src/main/java/example/SecurityConfig.java
+++ docker_example_springboot/demo/src/main/java/example/SecurityConfig.java
@@ -1,93 +1,45 @@
 package example;
 
-import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
-import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
-import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
-import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy;
-import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
 @Configuration
 @EnableWebSecurity
-@KeycloakConfiguration
 public class SecurityConfig {
-
-    @Value("${spring.security.oauth2.client.registration.keycloak.client-id}")
-    private String clientId;
-
-    @Value("${spring.security.oauth2.client.registration.keycloak.client-secret}")
-    private String clientSecret;
-
-    @Value("${spring.security.oauth2.client.registration.keycloak.scope}")
-    private String[] scopes;
-
-    @Value("${spring.security.oauth2.client.provider.keycloak.authorization-uri}")
-    private String authorizationUri;
-
-    @Value("${spring.security.oauth2.client.provider.keycloak.token-uri}")
-    private String tokenUri;
-
-    @Value("${spring.security.oauth2.client.provider.keycloak.user-info-uri}")
-    private String userInfoUri;
-
-    @Value("${spring.security.oauth2.client.provider.keycloak.user-name-attribute}")
-    private String userNameAttribute;
-
-    @Value("${spring.security.oauth2.client.registration.keycloak.redirect-uri}")
-    private String redirectUri;
-
-    @Value("${spring.security.oauth2.client.registration.keycloak.client-name}")
-    private String clientName;
-    
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.cors(Customizer.withDefaults()); // Enable CORS
-        http.csrf(csrf -> csrf.disable()); // Disable CSRF
-
         http
-            .authorizeHttpRequests(authorizeRequests -> authorizeRequests // Require all requests to be authorized
-                .requestMatchers("/admin/**").hasRole("admin")
-                .requestMatchers("/user/**").hasRole("user")
-                .anyRequest().authenticated()
+            .csrf().disable()// Disable CSRF protection for testing purposes
+            .authorizeHttpRequests(authorizeRequests ->
+                authorizeRequests
+                    .requestMatchers("/admin/**").hasRole("admin")
+                    .requestMatchers("/user/**").hasRole("user")
+                    .anyRequest().authenticated()
             )
-            .oauth2Login(Customizer.withDefaults()); // Enable OAuth2 login
-        http.oauth2ResourceServer().jwt();
+            .oauth2Login(withDefaults())
+            .oauth2ResourceServer(oauth2ResourceServer ->
+                oauth2ResourceServer.jwt(jwt ->
+                    jwt.decoder(jwtDecoder())
+                )
+            )
+            .sessionManagement(sessionManagement ->
+                sessionManagement
+                    .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED) // Ensure session is created if required
+            );
 
         return http.build();
     }
 
     @Bean
-    public ClientRegistrationRepository clientRegistrationRepository() {
-        return new InMemoryClientRegistrationRepository(keycloakClientRegistration());
-    }
-
-    private ClientRegistration keycloakClientRegistration() {
-        return ClientRegistration.withRegistrationId("keycloak")
-                .clientId(clientId)
-                .clientSecret(clientSecret)
-                .scope(scopes)
-                .authorizationUri(authorizationUri)
-                .tokenUri(tokenUri)
-                .userInfoUri(userInfoUri)
-                .userNameAttributeName(userNameAttribute)
-                .clientName(clientName)
-                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-                .redirectUri(redirectUri)
-                .build();
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withJwkSetUri("http://localhost:4000/realms/backend/protocol/openid-connect/certs").build();
     }
 }
